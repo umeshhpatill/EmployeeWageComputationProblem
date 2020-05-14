e33357d490fa99d9d33f46222285545f0eb1b47e
umesh <usp5758@gmail.com>
2020-05-14 10:14:13 +0530

    case UC5 pushed

--- EmpWage.sh
+++ EmpWage.sh
@@ -5,20 +5,27 @@ presenty=$((RANDOM % 3))
 isPartTime=1
 isFullTime=2
 salaryPerHr=20
+totalSalary=0
+workDays=20
 
-case $presenty in
-	$isPartTime)
-		echo "Present for part time"
-		workHrs=4
-		;;
-	$isFullTime)
-		echo "Present for Full Time"
-		workHrs=8
-		;;
-	*)
-		echo "Absent"
-		;;
-esac
+for ((day=1; day<=$workDays; day++))
+do
+	presenty=$((RANDOM % 3))
+	case $presenty in
+		$isPartTime)
+			echo "Present for part time"
+			workHrs=4
+			;;
+		$isFullTime)
+			echo "Present for Full Time"
+			workHrs=8
+			;;
+		*)
+			echo "Absent"
+			;;
+	esac
+	salary=$((workHrs * salaryPerHr))
+	totalSalary=$(($totalSalary + $salary))
+done
 
-salary=$((workHrs * salaryPerHr))
-echo "Salary: "$salary
+echo "Total Salary: "$totalSalary
