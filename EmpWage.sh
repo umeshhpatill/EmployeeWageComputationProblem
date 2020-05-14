a4a79034efd96bd781b6d3fb6b92bd27fca23ad2
umesh <usp5758@gmail.com>
2020-05-14 10:17:36 +0530

    case UC6 pushed

--- EmpWage.sh
+++ EmpWage.sh
@@ -7,25 +7,30 @@ isFullTime=2
 salaryPerHr=20
 totalSalary=0
 workDays=20
+totalHrs=0
+days=0
 
-for ((day=1; day<=$workDays; day++))
+while [ $totalHrs -le 100 -a $days -le 20 ]
 do
 	presenty=$((RANDOM % 3))
 	case $presenty in
 		$isPartTime)
 			echo "Present for part time"
 			workHrs=4
+			$((days++))
 			;;
 		$isFullTime)
 			echo "Present for Full Time"
 			workHrs=8
+			$((days++))
 			;;
 		*)
 			echo "Absent"
 			;;
 	esac
+	totalHrs=$(($totalHrs + $workHrs))
 	salary=$((workHrs * salaryPerHr))
 	totalSalary=$(($totalSalary + $salary))
 done
-
+echo "Total Hours: "$totalHrs
 echo "Total Salary: "$totalSalary
